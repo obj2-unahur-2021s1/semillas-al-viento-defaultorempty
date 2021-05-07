0a38bacd420e091aff0e292f36f6bb05399dfb2b
sdelafuente <coalsan@gmail.com>
2021-05-06 23:23:58 -0300

    v2. Comentarios.

--- src/main/kotlin/ar/edu/unahur/obj2/semillasAlViento/Parcela.kt
+++ src/main/kotlin/ar/edu/unahur/obj2/semillasAlViento/Parcela.kt
@@ -2,21 +2,28 @@ package ar.edu.unahur.obj2.semillasAlViento
 
 class Parcela(val ancho: Int, val largo: Int, val horasSolPorDia: Int) {
   val plantas = mutableListOf<Planta>()
+
+  // Redundancia mínima. El total de plantas es la cantidad de
+  // elementos dentro de la lista de plantas en la parcela
   var cantidadPlantas = 0
 
   fun superficie() = ancho * largo
-  //Redundancia mínima, se repite varias veces la formular ancho * largo cuando
+
+  //Simplicidad y Redundancia mínima, se repite varias veces la formular ancho * largo cuando
   //Se podría llamar a superficie() para resolverlo
   fun cantidadMaximaPlantas() =
     if (ancho > largo) ancho * largo / 5 else ancho * largo / 3 + largo
 
   fun plantar(planta: Planta) {
+    // Simplicidad
     if (cantidadPlantas == this.cantidadMaximaPlantas()) {
       println("Ya no hay lugar en esta parcela")
     } else if (horasSolPorDia > planta.horasDeSolQueTolera() + 2) {
       println("No se puede plantar esto acá, se va a quemar")
     } else {
       plantas.add(planta)
+
+      // Redundancia mínima
       cantidadPlantas += 1
     }
   }
@@ -24,6 +31,10 @@ class Parcela(val ancho: Int, val largo: Int, val horasSolPorDia: Int) {
 
 class Agricultora(val parcelas: MutableList<Parcela>) {
   var ahorrosEnPesos = 20000
+
+  //simplicidad / YAGNI
+  // En el enunciado se dice que la tierra no puede ser comprada ni vendida
+  // por lo tanto tenemos un método que nunca se va a usar
 
   // Suponemos que una parcela vale 5000 pesos
   fun comprarParcela(parcela: Parcela) {
@@ -41,9 +52,14 @@ class Agricultora(val parcelas: MutableList<Parcela>) {
     }
 
   fun plantarEstrategicamente(planta: Planta) {
+    // Errores de Mutaciónes controladas
+    // pasable a una función de obtener la parecela elegida
     val laElegida = parcelas.maxBy { it.cantidadMaximaPlantas() - it.cantidadPlantas }!!
 
-    //Robustez. No usa el método de plantar, por lo cual evita la validación que posee.
+
+    // No usa el método de plantar de la parcela,
+    // por lo cual evita la validación que posea
+    // y accede directamente al atributo plantas.
     laElegida.plantas.add(planta)
   }
 }
--- src/main/kotlin/ar/edu/unahur/obj2/semillasAlViento/Planta.kt
+++ src/main/kotlin/ar/edu/unahur/obj2/semillasAlViento/Planta.kt
@@ -1,9 +1,13 @@
 package ar.edu.unahur.obj2.semillasAlViento
 
+// Mutación controlada
+// la altura de la planta está marcada como var, pero en
+// este ejercicio la altura va a ser inmutable para cada planta
+// lo cual deberia ser val altura: Float
 abstract class Planta(val anioObtencionSemilla: Int, var altura: Float) {
   fun esFuerte() = this.horasDeSolQueTolera() > 10
 
-  //(Des)acoplamiento
+  //Cohesion
   //Por que un método de parcela depende de la planta?
   fun parcelaTieneComplicaciones(parcela: Parcela) =
     parcela.plantas.any { it.horasDeSolQueTolera() < parcela.horasSolPorDia }
@@ -17,6 +21,8 @@ class Menta(anioObtencionSemilla: Int, altura: Float) : Planta(anioObtencionSemi
   override fun daSemillas() = this.esFuerte() || altura > 0.4
 }
 
+// Simplicidad + Cohesion a nivel de clase, se podía separar en una clase
+// SojaTransgénica
 class Soja(anioObtencionSemilla: Int, altura: Float, val esTransgenica: Boolean) : Planta(anioObtencionSemilla, altura) {
   override fun horasDeSolQueTolera(): Int  {
     // ¡Magia de Kotlin! El `when` es como un `if` pero más poderoso:
@@ -27,15 +33,18 @@ class Soja(anioObtencionSemilla: Int, altura: Float, val esTransgenica: Boolean)
       else          -> 9
     }
 
+    // Si se ivide las clase Soja en 2, la clase
+    // soja transgenica el return sería horasDeSolQueTolera * 2
     return if (esTransgenica) horasBase * 2 else horasBase
   }
 
-
+  // Cohesion a nivel de método
   override fun daSemillas(): Boolean  {
     if (this.esTransgenica) {
       return false
     }
 
+    // Cohesion, no debería hacer tanta lógica en un return
     return this.esFuerte() || (this.anioObtencionSemilla > 2007 && this.altura > 1)
   }
 }
